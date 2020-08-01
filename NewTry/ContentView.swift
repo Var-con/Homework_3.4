2bea7fdcf95fafdd2e41f59ba62c41ca7614ef03
Stanislav Klimov <kpuctallaz@gmail.com>
2020-08-01 17:18:02 +0500

    Initial Commit

---
+++ NewTry/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewTry
+//
+//  Created by Станислав Климов on 01.08.2020.
+//  Copyright © 2020 Stanislav Klimov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
